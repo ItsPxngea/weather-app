caa60b2278aa8c04fc9ca527a5347773ddedc43f
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-08 14:45:43 +0200

    Added a Search bar for being able to search across any country

--- src/App.js
+++ src/App.js
@@ -10,42 +10,29 @@ function WeatherApp() {
     wind: 10,
     precipitation: 20,
 })*/
-  //const [weather,setWeather]=useState(null);
+  
   const [error, setError] = useState(null);
   const [loading, setLoading]=useState(true);
-  const [capetownWeather,setCapeTownWeather]=useState(null);
+  //const [capetownWeather,setCapeTownWeather]=useState(null);
   const [johannesburgWeather,setJohannesburgWeather]=useState(null);
+  const [searchCity, setSearchCity] = useState("");
+  const [searchResult, setSearchResult] = useState(null);
 
-  /*useEffect(()=> {
-    fetch("http://localhost:5128/api/weather")
-    .then((res)=>{
-      if(!res.ok)throw new Error("Failed to fetch weather");
-      return res.json();
-    })
-    .then((data)=>{
-      setWeather(data);
-      setLoading(false);
-    })
-    .catch((err)=>{
-      setError(err.message);
-      setLoading(false);
-    });
-  });
-  */
-
+  
   useEffect(() => {
     const fetchWeather = async () => {
       try {
-        const[ct,jhb]=await Promise.all([
-          fetch("http://localhost:5128/api/weather/Cape Town, ZA").then(r=>r.json()),
-          fetch("http://localhost:5128/api/weather/Johannesburg").then(r=>r.json())
+        const[jhb]=await Promise.all([
+          //cities.map(city=>fetch(`http://localhost:5128/api/weather/${encodeURIComponent(city)}`).then(r=>r.json())),
+          //fetch(`http://localhost:5128/api/weather/${encodeURIComponent('Cape Town, Western Cape')}`).then(r=>r.json()),
+          fetch(`http://localhost:5128/api/weather/${encodeURIComponent('Johannesburg, Western Cape')}`).then(r=>r.json())
         ]);
-        setCapeTownWeather(ct);
+        //setCapeTownWeather(ct);
         setJohannesburgWeather(jhb);
         
       }catch(e){
         setError("Failed to fetch city weather");
-        //setLoading(false);
+        
       }finally{
         setLoading(false);
       }
@@ -54,7 +41,23 @@ function WeatherApp() {
       fetchWeather();
   },[]);
 
-  //if(loading) return <p className="loadingscreen">Loading...</p>;
+  const handleSearch = async () => {
+    if(!searchCity.trim())return;
+    setSearchResult(null);
+  
+    try{
+      const res=await fetch(`http://localhost:5128/api/weather/${encodeURIComponent(searchCity)}`);
+      if (!res.ok) throw new Error("City not found during search");
+      const data=await res.json();
+      setSearchResult(data);
+    }
+     catch(e){
+      setError("Please enter a valid city name");
+     }finally{
+      setLoading(false);
+     }
+  };
+
   if (loading) return (
     <div className="loading-container">
         <div className="spinner"></div>
@@ -72,31 +75,48 @@ if (loading) return (
 
   return (
     <>
+    <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search city..."
+          value={searchCity}
+          onChange={(e) => setSearchCity(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+        />
+        <button className="search-button" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+
     <h1 className="weather-title">Local Weather</h1>
+    {searchResult && (
+      <div className="weather-container">
+        <div className="weather-card">
+          <h2>{searchResult?.location}</h2>
+          <p className="temp">{searchResult?.temp}°C</p>
+          <p>{searchResult?.condition}</p>
+          <p>Humidity: {searchResult?.humidity}%</p>
+          <p>Wind: {searchResult?.windSpeed} km/h</p>
+          <p>Precipitation: {searchResult?.precipitation}%</p>
+        </div>
+    </div>
+    )}
     <div className="weather-container">
       <div className="weather-card">
-        <h2>{capetownWeather.location}</h2>
-        <p className="temp">{capetownWeather.temp}°C</p>
-        <p>{capetownWeather.condition}</p>
-        <p>Humidity: {capetownWeather.humidity}%</p>
-        <p>Wind: {capetownWeather.windSpeed} km/h</p>
-        <p>Precipitation: {capetownWeather.precipitation}%</p>
-      </div>
-    
-
-    
-      <div className="weather-card">
-        <h2>{johannesburgWeather.location}</h2>
-        <p className="temp">{johannesburgWeather.temp}°C</p>
-        <p>{johannesburgWeather.condition}</p>
-        <p>Humidity: {johannesburgWeather.humidity}%</p>
-        <p>Wind: {johannesburgWeather.windSpeed} km/h</p>
-        <p>Precipitation: {johannesburgWeather.precipitation}%</p>
+        <h2>{johannesburgWeather?.location}</h2>
+        <p className="temp">{johannesburgWeather?.temp}°C</p>
+        <p>{johannesburgWeather?.condition}</p>
+        <p>Humidity: {johannesburgWeather?.humidity}%</p>
+        <p>Wind: {johannesburgWeather?.windSpeed} km/h</p>
+        <p>Precipitation: {johannesburgWeather?.precipitation}%</p>
       </div>
     </div>
+    
     
     </>
   );
 }
+
 
 export default WeatherApp;
